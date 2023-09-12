4d698cf507b04870fe96bb17dc79eeab82604c09
Claire Delosa <clair650@gmail.com>
2023-09-12 18:05:47 +1000

    Crowdfunding front end

---
+++ crowdfunding-frontend/src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import { Link, Outlet } from "react-router-dom";
+import "./NavBar.css";
+import Logo from '../assets/icons/logo.svg'
+
+function NavBar() {
+    return (
+    <nav className= 'navbar'>
+        <div className= 'container'>
+            <div className='logo'>
+                <img src={Logo} alt="Logo" />
+            </div>
+            <div className='nav-elements'>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                        <Link to="/login">Log In</Link>
+                    </li>
+                </ul>  
+            </div>   
+        </div>
+        <Outlet /> 
+    </nav>
+    );
+    }
+
+export default NavBar;
