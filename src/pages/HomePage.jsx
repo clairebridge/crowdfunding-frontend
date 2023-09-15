ae07fe3e47f30d7cc1e3af041fd83d44a08e9edd
Claire Delosa <clair650@gmail.com>
2023-09-16 09:50:46 +1000

    fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,8 +5,7 @@ import "./HomePage.css";
 function HomePage() {
     const { projects } = useProjects();
     return (
-        <div id="project-list">
-            <link to="/">Create Project</link> 
+        <div id="project-list"> 
             {projects.map((projectData, key) => {
                 return <ProjectCard key={key} projectData={projectData} />;
             })}
